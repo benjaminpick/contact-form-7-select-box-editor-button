a1f6f1d0f3abe0fb834cd7b1e9c09463ff622373
Benjamin Pick <benjaminpick@github.com>
2013-11-01 08:44:41 +0100

    unit

--- bin/activate_plugin.php
+++ bin/activate_plugin.php
@@ -7,6 +7,13 @@ define('ABSPATH', $abs);
 require_once ABSPATH.'wp-admin/includes/plugin.php';
 require_once ABSPATH.'wp-admin/includes/plugin-install.php';
 
+if ($_SERVER['argc'] < 2)
+	die('Usage: php activate_plugin.php basename filename.php');
+
+$basename = $_SERVER['argv'][0];
+$filename = $_SERVER['argv'][1];
+
+
 if (is_plugin_active( $basename ) || is_plugin_active_for_network( $basename ))
 	die('Plugin already active.');
 	
